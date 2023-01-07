0a84b090d449392f4549daed5009944f4a325b25
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-07 06:01:33 -0500

    Create vpc module

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc-blog" {
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev-vpc"
@@ -44,7 +44,7 @@ module "sblog_sg" {
   version = "4.16.2"
   name = "blog_new"
 
-  vpc_id = module.vpc-blog.public_subnets[0]
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
