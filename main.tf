14191fd8884732a384c8c5afa8037526adb4497b
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-07 08:43:42 -0500

    create module vpc

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "sblog_sg" {
   
   name = "blog"
 
-  vpc_id = module.vpc
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
