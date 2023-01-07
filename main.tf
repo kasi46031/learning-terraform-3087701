ae612deac638f268c4831b1f3b417d7bc7db2bd0
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-07 05:18:13 -0500

    Use module for security group

--- main.tf
+++ main.tf
@@ -18,12 +18,28 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.sblog_sg.security_group_id]
+  #vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Terraform in speed"
   }
 }
+
+module "sblog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.2"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 
 resource "aws_security_group" "blog" {
   name = "blog"
@@ -32,7 +48,17 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }   
 
-resource "aws_security_group_rule" "httpin" {
+resource "aws_security_group_rule" "httpin80" {
+  type = "ingress"
+  from_port      = 80
+  to_port        = 80
+  protocol       = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+
+}
+resource "aws_security_group_rule" "httpin443" {
   type = "ingress"
   from_port      = 443
   to_port        = 443
