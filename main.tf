0868e4af1a2818c06db6f95673d9f3ebcb16f959
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-07 06:50:21 -0500

    Create vpc subnets

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ module "vblog_vpc" {
   name = "dev-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-west-2a"]
+  public_subnets  = ["10.0.101.0/24"]
 
 
   tags = {
@@ -30,11 +30,10 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  
   vpc_security_group_ids = [module.sblog_sg.security_group_id]
 
   tags = {
-    Name = "Terraform in speed"
+    name = "Terraform in speed"
   }
 }
 
@@ -44,7 +43,7 @@ module "sblog_sg" {
   name = "blog"
 
   vpc_id = module.vblog_vpc.public_subnets[0]
-  
+
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
