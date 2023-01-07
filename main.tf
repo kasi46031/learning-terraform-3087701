2b42cd2ca60d1b5637a60ffa34ac62a1f430768c
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-07 08:22:31 -0500

    Create module vpc

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ module "vblog_vpc" {
   name = "dev-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a"]
-  public_subnets  = ["10.0.101.0/24"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
 
   tags = {
@@ -25,17 +25,6 @@ module "vblog_vpc" {
   }
 }
 
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.sblog_sg.security_group_id]
-
-  tags = {
-    name = "Terraform in speed"
-  }
-}
 
 module "sblog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -50,3 +39,18 @@ module "sblog_sg" {
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
+
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.sblog_sg.security_group_id]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
