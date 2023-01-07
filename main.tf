963656dc494d3e4e6bff9d457b5f4dad39c2bac4
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-07 08:56:40 -0500

    Create module vpc

--- main.tf
+++ main.tf
@@ -28,11 +28,11 @@ module "vpc" {
 
 module "sblog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version  = "4.13.0"
+  
 
   name = "blog"
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -46,7 +46,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.sblog_sg.blog]
+  vpc_security_group_ids = [module.sblog_sg.vpc_security_group_ids]
 
   tags = {
     Terraform = "true"
