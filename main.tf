a4e37f865bc8d037403c6a458f91adea0c85af56
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-08 15:42:46 -0500

    create module alb

--- main.tf
+++ main.tf
@@ -66,8 +66,7 @@ module "blog_alb" {
 
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_groups.id]
-
+  
   access_logs = {
     bucket = "my-alb-logs"
   }
