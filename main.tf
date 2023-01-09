82d0fc8824e3a8f4a724ec01b2bacd16af750bf4
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-09 11:26:55 -0500

    create dynamodb table

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
-module "dynamodb_table" {
-  source   = "terraform-aws-modules/dynamodb-table/aws"
+resource "aws_dynamodb_table" "this" {
+ # source   = "terraform-aws-modules/dynamodb-table/aws"
 
   name      = var.name
   hash_key  = var.hashkey
   range_key = var.rangekey
-
-  point_in_time_recovery_enabled  = true
-  server_side_encryption_enabled = true
+  
+  point_in_time_recovery {
+    enabled = true
+  }
+  
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.this.arn
+  }
   
 
   dynamic "attribute" {
@@ -22,6 +28,11 @@ module "dynamodb_table" {
     Environment = "Dev"
   }
   
+}
+
+resource "aws_kms_key" "this" {
+  description  = var.kmskey
+  multi_region = false
 }
 
 
