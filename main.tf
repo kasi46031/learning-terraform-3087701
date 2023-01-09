ed9174d47038a6f4f0d835e0ed9a293015e212b4
kasi46031 <121393771+kasi46031@users.noreply.github.com>
2023-01-08 22:05:36 -0500

    create module dynamodb

--- main.tf
+++ main.tf
@@ -1,94 +1,22 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+module "dynamodb_table" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
+  name     = "my-table"
+  hash_key = "id"
 
-  owners = ["979382823631"] # Bitnami
-}
-
-
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c" ]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  name = "blog-sg"
-
-  vpc_id = module.blog_vpc.vpc_id
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-module "blog_asg" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-
-  # Autoscaling group
-  name = "blog-asg"
-
-  min_size                  = 1
-  max_size                  = 2
-  
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  
-  image_id = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  
-}
-
-
-module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  name = "blog-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
-
-
-  target_groups = [
+  attributes = [
     {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      
+      name = "id"
+      type = "N"
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  point_in_time_recovery {
+    enabled = true
+  }
 
   tags = {
+    Terraform   = "true"
     Environment = "Dev"
   }
 }
-
